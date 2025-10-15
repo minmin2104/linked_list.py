5ce38a6ca04fbc7b2d98781494baf8355c9c5412
minmin2104 <aminkafri0045@gmail.com>
2025-10-15 10:44:45 +0800

    just playing with unittest

---
+++ tests/test_moretest.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestSplit(unittest.TestCase):
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
