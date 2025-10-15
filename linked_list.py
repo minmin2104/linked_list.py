20c225d49056f3ffbd3aa07ff07f59c23f9da3b2
minmin2104 <aminkafri0045@gmail.com>
2025-10-15 11:07:28 +0800

    delete main.py, layout the function for linked list

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,27 @@
-class LinkedList:
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class SingularLinkedList:
     def __init__(self):
         self.__size = 0
+
+    def insert():
+        pass
+
+    def delete():
+        pass
+
+    def search():
+        pass
+
+    def update():
+        pass
+
+    def is_empty(self) -> bool:
+        return self.__size == 0
 
     def size(self):
         return self.__size
--- main.py
+++
@@ -1,7 +0,0 @@
-class Main:
-    def __init__(self):
-        print('hello, world!')
-
-
-if __name__ == '__main__':
-    Main()
