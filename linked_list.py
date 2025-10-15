ca62ce1d7b877c455dafb8591f3ff4582e536974
minmin2104 <aminkafri0045@gmail.com>
2025-10-15 11:26:06 +0800

    implement insert first & test insert first

--- linked_list.py
+++ linked_list.py
@@ -6,10 +6,32 @@ class Node:
 
 class SingularLinkedList:
     def __init__(self):
+        self.__head = None
+        self.__tail = None
         self.__size = 0
 
-    def insert():
+    def insert_first(self, value):
+        if self.__head is not None:
+            new_node = Node(value)
+            new_node.next = self.__head
+            self.__head = new_node
+            self.__size += 1
+        else:
+            self.__head = Node(value)
+            self.__tail = self.__head
+            self.__size += 1
+
+    def insert_last(self, value):
         pass
+
+    def insert(self, index, value):
+        pass
+
+    def first(self):
+        return self.__head.value
+
+    def last(self):
+        return self.__tail.value
 
     def delete():
         pass
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,12 +1,19 @@
 import unittest
 
-from linked_list import LinkedList
+from linked_list import SingularLinkedList
 
 
 class TestLinkedList(unittest.TestCase):
 
     def setUp(self):
-        self.linked_list = LinkedList()
+        self.ll = SingularLinkedList()
 
-    def test_get_size(self):
-        self.assertIsNotNone(self.linked_list.size())
+    def test_insert_first(self):
+        self.ll.insert_first(5)
+        self.assertEqual(self.ll.first(), 5)
+        self.ll.insert_first(8)
+        self.assertEqual(self.ll.first(), 8)
+        self.ll.insert_first(12)
+        self.assertEqual(self.ll.first(), 12)
+        self.assertEqual(self.ll.last(), 5)
+        self.assertEqual(self.ll.size(), 3)
